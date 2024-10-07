1fe6480ab7ff0e4bbec0a5476313ebb8eb78d1d2
Yudobshere12 <yudobsheremijares@gmail.com>
2024-10-07 12:19:30 +0800

    added process and output

--- methods.js
+++ methods.js
@@ -3,3 +3,9 @@ const person = {
     age: 19,
     gender: "Female"
    }
+  //Process and Output
+  console.log("Hello love");
+  console.log("Name: " + person.name);
+  console.log("Age: "  +person.age);
+  console.log("Gender: " + person.gender);  
+  console.log("-----------------------------------");
