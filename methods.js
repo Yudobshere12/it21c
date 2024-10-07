08562126fe5d365b98cf6a99ccd25a7b42377fd7
Yudobshere12 <yudobsheremijares@gmail.com>
2024-10-07 12:22:58 +0800

    added solution 2

--- methods.js
+++ methods.js
@@ -17,10 +17,21 @@ class Person {
         this.age = age;
         this.gender = gender;
     }
-    
+
      //Solution 1: Display via console.log
      displayInfo() { //Process and Output
         console.log("Name: " + this.name);
         console.log("Age: "  + this.age);
         console.log("Gender: " + this.gender);
      }
+     
+      //Solution 2: Display via HTML
+    displayViaHTML() {
+        const personElement = document.getElementById('person1');
+        personElement.innerHTML = `
+            <strong>Name:</strong> ${this.name}<br>
+            <strong>Age:</strong> ${this.age}<br>
+            <strong>Gender:</strong> ${this.gender}
+        `;
+    }
+}
